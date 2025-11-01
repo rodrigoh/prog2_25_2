40e3cbad9627b1ba9a919a50fe7c091f71dd38e7
Rodrigo Henrich <rodrigohenrich@faccat.br>
2025-10-31 21:28:49 -0300

    Ajustes na classe Conta (exercício)

--- src/aula12/banco/Conta.java
+++ src/aula12/banco/Conta.java
@@ -1,24 +1,31 @@
 package aula12.banco;
 
+import java.util.Random;
+
 public class Conta {
   String titular;
   String identificador;
   float saldo;
+  String senha;
 
   Conta(){
     saldo = 0;
+    geraIdentificador();
   }
 
-  Conta(String titular, String identificador){
+  Conta(String titular, String senha){
     this.titular = titular;
-    this.identificador = identificador;
+    this.senha = senha;
+    geraIdentificador();
     saldo = 0;
   }
 
-  Conta(String titular, String identificador, float saldoInicial){
-    this.titular = titular;
-    this.identificador = identificador;
-    saldo = saldoInicial;
+  void geraIdentificador(){
+    Random aleatorio = new Random();
+    identificador = "";
+    for (int i = 0; i < 4; i++)
+      identificador+=(char)aleatorio.nextInt(65,90);
+    identificador+=aleatorio.nextInt(10000, 99999);
   }
 
   void depositar(float valor){
@@ -31,6 +38,10 @@ public class Conta {
       return true;
     }
     return false;
+  }
+
+  boolean validaAcesso(String identificador, String senha){
+    return this.identificador.equals(identificador) && this.senha.equals(senha);
   }
 
   String mostraDados(){
